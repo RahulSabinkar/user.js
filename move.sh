a03e0422bd3a5c8b7d5720cbfe5681f9a857686c
Rahul Sabinkar <rahulsabinkar4u@gmail.com>
2020-11-10 03:15:52 +0000

    added colors

--- move.sh
+++ move.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/sh
+# Colors used for printing
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+NC='\033[0m' # No Color
 
-printf "Enter exact name of your firefox profile (case sensitive): "
+printf "${GREEN}Enter exact name of your firefox profile (case sensitive): ${NC}"
 read profile
+
 currdir=$(pwd)
 tardir=$HOME/.mozilla/firefox/*.$profile/
-cd $tardir && tardir=$(pwd) && cd $currdir
-printf "Copying files from $currdir to $tardir\n"
-printf "Confirm? yes or no: "
-read answer
-if [[ -d $tardir ]] ; then
 
-    if [[ "$answer" == "yes" || "$answer" == "y" ]] ; then
+cd $tardir && tardir=$(pwd) && cd $currdir
+
+printf "${ORANGE}Copying files from ${NC}$currdir${ORANGE} to ${NC}$tardir\n${NC}"
+printf "${GREEN}Do you confirm? yes or no: ${NC}"
+read answer
+
+if [[ "$answer" == "yes" || "$answer" == "y" ]] ; then
+    if [[ -d $tardir ]] ; then
         cp -iv \
             prefsCleaner.sh \
             updater.sh \
@@ -18,11 +26,11 @@ if [[ -d $tardir ]] ; then
             user-overrides.js \
             $tardir
     else
-        printf "Process terminated by user\n"
+        printf "${RED}Error: $tardir not found. Please enter proper name of your firefox profile.\n${NC}"
         exit 1
     fi
 
-    printf "Do you want to run updater.sh? yes or no: "
+    printf "${GREEN}Do you want to run updater.sh? yes or no: ${NC}"
     read answer2
 
     if [[ "$answer2" == "yes" || "$answer2" == "y" ]] ; then
@@ -30,11 +38,10 @@ if [[ -d $tardir ]] ; then
             sh updater.sh
         cd $currdir
     else
-        printf "user-overrides.js was not updated"
+        printf "${RED}user-overrides.js was not updated\n${NC}"
         exit 1
     fi
-
 else
-    printf "Error: $tardir not found. Please enter proper name of your firefox profile.\n"
+    printf "${RED}Process terminated by user\n${NC}"
     exit 1
 fi
